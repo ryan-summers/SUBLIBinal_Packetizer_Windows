9bc9b344b27753c3923e33e83f6b6553ad691a11
ryan-summers <summers.ryan.m@gmail.com>
2015-12-31 15:41:50 -0800

    Addition of GPL v2 License

--- include/packetInterpretter.h
+++ include/packetInterpretter.h
@@ -1,17 +1,31 @@
-//Packetizer Header
-
 #ifndef PACKETIZER_H
 #define PACKETIZER_H
 
+/*
+	SUBLIBinal_Packetizer_Windows - GNU GPLv2 Notice
+    Copyright (C) 2015  Ryan Summers and James Irwin
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
 #include <stdio.h>
 #include <Windows.h>
 #include <WinBase.h>
+#include "serial.h"
 
 
 //Forward Declarations
 
 void acquireSynchronization(HANDLE portHandle, char control);
-
 
 void getSync (HANDLE portHandle, char controlByte);
 int readPacket(HANDLE portHandle, char control, char *data);
--- src/serial.c
+++ src/serial.c
@@ -1,5 +1,21 @@
 
-#include "Serial.h"
+#include "serial.h"
+
+/*
+	SUBLIBinal_Packetizer_Windows - GNU GPLv2 Notice
+    Copyright (C) 2015  Ryan Summers and James Irwin
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 int configureSerial(HANDLE port_handle, int baudrate)
 {
